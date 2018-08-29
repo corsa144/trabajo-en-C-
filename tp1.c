108d2ffbcd3aee2de8b46536a453dbdd0b7acbca
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-08-28 22:11:20 -0300

    TP1
    esqueleto, revisar

---
+++ tp1.c
@@ -0,0 +1,34 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+
+    int main(){
+        int opcion;
+        float op1,op2;
+        do{
+                system("cls");
+                printf("menu");
+                printf("\n\n1.ingresar primer operando(a=%.2f)",op1)
+                printf("ingrese numero 1 \n ingrese numero 2:");
+                printf("")
+                switch(opcion){
+                    case 1:
+
+                        break;
+                }
+        }while(opcion!=5);
+
+
+
+
+
+
+
+    }
+    int menu(){//3 menu
+        printf("");
+        printf("");
+        printf("seleccione:");
+        scanf("%d",&opcion);
+
+
+    }
