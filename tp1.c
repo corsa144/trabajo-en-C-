5e3841c6fc8c5b6f298a8e774e966a0630302fca
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-08-28 22:18:51 -0300

    TP1
    esqueleto

--- tp1.c
+++ tp1.c
@@ -9,13 +9,27 @@
                 printf("menu");
                 printf("\n\n1.ingresar primer operando(a=%.2f)",op1)
                 printf("ingrese numero 1 \n ingrese numero 2:");
-                printf("")
+                printf("\n3.calcular todas las operaciones");
+                prentf("\4.mostrar todos los resultados");
+                printf("seleccionar opcion:");
+                scanf("%d",&opcion);
                 switch(opcion){
                     case 1:
 
                         break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
                 }
         }while(opcion!=5);
+        return 0;
 
 
 
