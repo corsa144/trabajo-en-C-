da93656b1af281ea95433b43042741b0bc5a9a16
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-09-10 22:18:07 -0300

    mas funciones
    for

--- cadena de caracteres.c
+++ cadena de caracteres.c
@@ -1,11 +1,12 @@
     #include <string.h>
     #include <stdio.h>
     #include <stdlib.h>
+    #include <ctype.h>
 
     int main()
 
     {
-         int cantidad;
+         int cantidad,i;
          char nombre[31];
          char apellido[31];
          char nomApe[62];
@@ -24,12 +25,29 @@
          //strcat(nomApe," ");
          //strcat(nomApe,nombre);
          //nomApe[0]='\0';
+
+
+         strlwr(apellido);
+         strlwr(nombre);
+         nombre[0]=toupper(nombre[0]);
+
+
+         for (i=0;i<strlen(nombre);i++){
+            if (nombre[i]==' '&&nombre[i+1]!='\0')
+            {
+                nombre[i+1]=toupper(nombre[i+1]);
+            }
+         }
+         apellido[0]=toupper(apellido[0]);
+         for (i=0;i<strlen(apellido);i++){
+            if (apellido[i]==' '&&apellido[i+1]!='\0'){
+                apellido[i+1]=toupper(apellido[i+1]);
+            }
+         }
          strcat(nomApe,apellido);
          strcat(nomApe," ");
          strcat(nomApe,nombre);
          printf("%s",nomApe);
-
-
          /*
          printf("ingrese nombre:");
          fflush(stdin);
